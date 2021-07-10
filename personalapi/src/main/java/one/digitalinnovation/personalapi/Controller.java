a57a2e1e729526a6ee85b6379c9ae11390293dfd
asouz <asouz@LAPTOP-REK1KDGC>
2021-07-10 12:30:37 -0300

    Projeto DIO com Spring

---
+++ personalapi/src/main/java/one/digitalinnovation/personalapi/Controller.java
@@ -0,0 +1,17 @@
+package one.digitalinnovation.personalapi;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/people")
+public class Controller {
+	
+	@GetMapping
+	public String getBook() {
+		return "API Test";
+	}
+		
+
+}
